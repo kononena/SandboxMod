362772b5868d2e6fb88a22213988205f4fa4764c
Aleksi Kononen <aleksikono@gmail.com>
2020-06-23 22:46:05 +0300

    Add Ichor Statue functionality

--- Items/IchorStatue.cs
+++ Items/IchorStatue.cs
@@ -3,37 +3,53 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ModLoader;
 using SandboxMod.Projectiles;
-using SandboxMod.Buffs;
 using static Terraria.ModLoader.ModContent;
 
 namespace SandboxMod.Items
 {
-	public class Sprite : ModItem
+	public class IchorStatue : ModItem
 	{
 		public override void SetStaticDefaults() 
 		{
-			DisplayName.SetDefault("Delicious goodness");
+			DisplayName.SetDefault("Ichor Statue");
+			Tooltip.SetDefault("Summons an ichor statue to fight for you\n" +
+				"It's everywhere");
+			ItemID.Sets.GamepadWholeScreenUseRange[item.type] = true;
+			ItemID.Sets.LockOnIgnoresCollision[item.type] = true;
 		}
 
 		public override void SetDefaults() 
 		{
-			item.CloneDefaults(ItemID.ZephyrFish);
-			item.width = 26;
-			item.height = 44;
-			item.shoot = ProjectileType<Projectiles.Sprite>();
-			item.buffType = BuffType<Buffs.Sprite>();
+			item.damage = 1;
+			item.knockBack = 1f;
+			item.mana = 10;
+			item.width = 20;
+			item.height = 20;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = Item.buyPrice(gold: 5);
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item44;
+
+			item.noMelee = true;
+			item.summon = true;
+			item.sentry = true;
+			item.shoot = ProjectileType<Projectiles.IchorStatue>();
 		}
+
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+			position = Main.MouseWorld;
+			player.UpdateMaxTurrets();
+			return true;
+        }
 
         public override void AddRecipes()
         {
 			ModRecipe rec = new ModRecipe(mod);
 			rec.SetResult(this);
 			rec.AddRecipe();
-        }
-
-        public override void UseStyle(Player player)
-        {
-			if (player.whoAmI == Main.myPlayer && player.itemTime == 0) player.AddBuff(item.buffType, 3600, true);
         }
 
     }
