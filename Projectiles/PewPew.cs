fe9d27948e809df4b1d4b8b2779b46535ce4e18a
kononena <67295524+kononena@users.noreply.github.com>
2020-06-23 20:18:32 +0300

    Commit initial files

---
+++ Projectiles/PewPew.cs
@@ -0,0 +1,59 @@
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using Steamworks;
+using System.Linq;
+
+namespace TutorialMod.Projectiles
+{
+	public class PewPew : ModProjectile
+	{
+		private bool closeToCursor;
+
+		public override void SetDefaults()
+		{
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.friendly = true;
+			projectile.ranged = true;
+			projectile.tileCollide = false;
+			projectile.light = 0.5f;
+			projectile.timeLeft = 300;
+		}
+
+        public override void AI()
+        {
+			Vector2 center = Main.MouseWorld;
+			Vector2 displacement = projectile.position - center;
+			float circleRadius = 100;
+
+			List<Vector2> directions = new List<Vector2>();
+			for (int i = 0; i < 200; i++)
+			{
+				NPC target = Main.npc[i];
+				if (!target.dontTakeDamage && !target.friendly && target.active)
+				{
+					directions.Add(projectile.position - target.position);
+				}
+			}
+
+			if (directions.Count > 0)
+			{
+				Vector2 newDir = directions.OrderBy(dir => dir.Length()).First();
+				projectile.velocity = -newDir / newDir.Length() * 16f;
+			}
+			else 
+            {
+				closeToCursor = displacement.Length() < circleRadius;
+				if (closeToCursor) projectile.velocity = displacement.RotatedBy(MathHelper.ToRadians(90)) / displacement.Length() * 5f;
+				else projectile.velocity = -displacement / displacement.Length() * 16f;
+            }
+
+			projectile.rotation = projectile.velocity.ToRotation() + MathHelper.ToRadians(90f);
+			projectile.spriteDirection = projectile.direction;
+		}
+    }
+}
---
+++ Tiles/SickFireBro.cs
@@ -0,0 +1,36 @@
+using Terraria.ID;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ObjectData;
+using Microsoft.Xna.Framework;
+
+namespace TutorialMod.Tiles
+{
+	public class SickFireBro2 : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileFrameImportant[Type] = true;
+			TileObjectData.newTile.CopyFrom(TileObjectData.Style1x1);
+			TileObjectData.newTile.Height = 1;
+			TileObjectData.newTile.Width = 1;
+			TileObjectData.newTile.CoordinateHeights = new[] { 16 };
+			TileObjectData.newTile.CoordinateWidth = 16;
+			TileObjectData.newTile.StyleHorizontal = true;
+			TileObjectData.addTile(Type);
+			AddMapEntry(Color.Blue);
+		}
+
+        public override bool NewRightClick(int i, int j)
+        {
+			Main.LocalPlayer.statLifeMax2 += 69;
+			Main.LocalPlayer.statLife += 69;
+			return true;
+        }
+
+        public override void NearbyEffects(int i, int j, bool closer)
+        {
+			Main.LocalPlayer.AddBuff(BuffID.OnFire, 119, true);
+        }
+    }
+}
